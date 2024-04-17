f274915233219a40877b8451ee6b05ef59f907c6
nikitayashny <95637235+nikitayashny@users.noreply.github.com>
2024-04-17 08:05:09 +0300

    lab23

---
+++ PSKP_Lab23/db.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+let phoneNumbers = require('./phoneNumbers');
+
+function commit() {
+    fs.writeFile(__dirname + '/phoneNumbers.json', JSON.stringify(phoneNumbers, null, 4), err => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+    });
+}
+
+module.exports =
+{
+    GetAll: () => { return phoneNumbers },
+
+    Add(field) {
+        const { name, number } = field;
+        if (name && number) {
+            phoneNumbers.push(
+                {
+                    name,
+                    number
+                });
+            commit();
+            return 1;
+        }
+        return 0;
+    },
+
+    Update(field) {
+        const { name, number } = field;
+
+        if (name, number) {
+            let isNumber = phoneNumbers.find(phone => phone.number == number);
+
+
+            isNumber.name = name;
+            isNumber.number = number;
+
+            commit();
+            return 1;
+        }
+        return 0;
+    },
+
+    Delete(name) {
+        if (name) {
+            let isName = phoneNumbers.find(phone => phone.name == name);
+            phoneNumbers = phoneNumbers.filter(phone => phone.name != name);
+            commit();
+            return 1;
+        }
+        return 0;
+    }
+};
